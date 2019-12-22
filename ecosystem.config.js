2cbabcc38f65c75b3a3bdd391ecad66c04ebad05
FPT Computer <huytocdo-fpt@vultr.guest>
2019-12-22 17:47:47 +0000

    config pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+        apps : [{
+                name: 'Zigvy-demo',
+                exec_mode: 'cluster',
+                instance: 'max',
+                cwd: './backend',
+                script: 'npm',
+                args: 'start:prod',
+        }]
+};
